af7089da759f03efd6d0a28e5f6c5edcf58e66b6
yeeun33 <iwreau@gmail.com>
2026-06-19 19:30:58 +0900

    되돌리기전

--- src/app.py
+++ src/app.py
@@ -34,7 +34,7 @@ from util.jobs.job_store import *
 from util.jobs.job_run import start_graph_pipeline_background, start_graph_update_pipeline_background
 from config.settings import *
 from util.user_path import UserPaths
-from util.database.db_reader import get_mail_stats, get_keyword_stats,get_mail_sync_stats,get_user_rating_stats,get_high_affinity_person_stats, get_keywords_by_person_date, get_mail_date_range, get_mail_exchange_stats, calculate_eis
+from util.database.db_reader import get_mail_stats, get_keyword_stats,get_mail_sync_stats,get_user_rating_stats,get_high_affinity_person_stats, get_keywords_by_person_date, get_mail_date_range, get_mail_exchange_stats, calculate_eis, get_date_range_person_stats
 
 
 from util.database.db_writer import (
